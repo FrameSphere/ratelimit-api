a5b956e6aa2bb8576a5c4cd325933935227beb69
KarolP-tech <karol.paschek@ferienwohnung-zornheim.de>
2026-04-05 12:39:20 +0200

    Fix

--- frontend/src/components/AlertsTab.tsx
+++ frontend/src/components/AlertsTab.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { api } from '../lib/api';
-import { PRO_FEATURES } from '../lib/plans';
+import { PRO_FEATURES, ProFeatureFlag } from '../lib/plans';
 
 interface AlertsTabProps {
   apiKeyId: number | null;
